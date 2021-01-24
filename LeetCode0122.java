3318569003243bcbe0e88436b60441967d516c8c
zyt153 <43976687+zyt153@users.noreply.github.com>
2021-01-24 16:12:46 +0800

    Add files via upload

---
+++ LeetCode0122.java
@@ -0,0 +1,24 @@
+/* Best Time to Buy and Sell Stock II
+ * Input: [7,1,5,3,6,4]
+ * Output: 7
+ * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5),
+                profit = 5-1 = 4.
+                Then buy on day 4 (price = 3) and sell on day 5 (price = 6),
+                profit = 6-3 = 3.
+ * */
+
+public class LeetCode0122 {
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;
+    }
+}
