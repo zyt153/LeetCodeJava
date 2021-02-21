f2b2f43a456988fa685951b7b8446b034c803e57
zyt153 <731509022@qq.com>
2021-02-21 22:56:26 +0800

    Add files via upload

---
+++ LeetCode0241.java
@@ -0,0 +1,51 @@
+/* Different Ways to Add Parentheses
+ * Input: "2*3-4*5"
+ * Output: [-34, -14, -10, -10, 10]
+ * Explanation:
+ * (2*(3-(4*5))) = -34
+ * ((2*3)-(4*5)) = -14
+ * ((2*(3-4))*5) = -10
+ * (2*((3-4)*5)) = -10
+ * (((2*3)-4)*5) = 10
+ * */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode0241 {
+    public static void main(String[] args) {
+        String input = "2-1-1";
+        System.out.println(diffWaysToCompute(input));
+    }
+
+    public static List<Integer> diffWaysToCompute(String input) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '+' || c == '-' || c == '*') {
+                List<Integer> left = diffWaysToCompute(input.substring(0, i));  // substring截取至第i-1个
+                List<Integer> right = diffWaysToCompute(input.substring(i + 1));
+                for (int l : left) {
+                    for (int r : right) {
+                        switch (c) {
+                            case '+':
+                                res.add(l + r);
+                                break;
+                            case '-':
+                                res.add(l - r);
+                                break;
+                            case '*':
+                                res.add(l * r);
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (res.isEmpty())
+            res.add(Integer.valueOf(input));
+
+        return res;
+    }
+}
